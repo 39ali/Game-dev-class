515ce96c99e9f31763631b6a8f3f1a7882264158
Ali Milhim <ali.milhim@powtoon.com>
2021-06-07 20:43:29 +0300

    Updated HW1 to work on macos

---
+++ HW2/Assets/Scripts/DoorMotion.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorMotion : MonoBehaviour
+{
+    private Animator anim;
+    private AudioSource sound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+     //   sound = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        anim.SetBool("isOpening", true);
+      //  sound.PlayDelayed(0.8f);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+        anim.SetBool("isOpening", false);
+       // sound.PlayDelayed(0.2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
