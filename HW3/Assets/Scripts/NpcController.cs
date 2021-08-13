6c15ade1681d53327c10eef9be142179937923a9
Ali Milhim <ali.milhim@powtoon.com>
2021-08-13 18:49:04 +0300

    fixed HW3

---
+++ HW3/Assets/Scripts/NpcController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcController : MonoBehaviour
+{
+
+	 
+	private npcMove[] npcs; 
+	private int currentNpc=0;
+	//move only once npc at time 
+	private bool oneIsWalking=false ;
+    // Start is called before the first frame update
+    void Start()
+    {
+       npcs = FindObjectsOfType<npcMove>();      
+    }
+
+   void Update()
+    {
+    	for (int i =0;i< npcs.Length;i++){
+    		if (npcs[i].tryStop()){
+    			oneIsWalking=false ;
+    			Debug.Log("making npc {" +i +"} stop");
+    		}		
+    	}
+
+		if (!oneIsWalking){
+			MoveNpc();
+			oneIsWalking=true ; 
+		}
+	    	
+    }
+
+    void MoveNpc(){
+
+    	//get the distenation index 
+    	int distenationsLnegth = npcs[0].GetDistenationLength();
+    	bool wasPicked = false  ; 
+    	int disIndex = 0; 
+    	while (!wasPicked){
+			disIndex = Random.Range(0,distenationsLnegth);
+
+			for (int i=0 ;i< npcs.Length;i++){
+				if (npcs[i].GetCurrentDistenationIndex()==disIndex){
+					wasPicked=false; 
+					break;
+				}
+				wasPicked=true ;
+			}
+    	}
+
+		int index = ++currentNpc%npcs.Length;
+		Debug.Log("making npc {" +index +"} walk");
+		npcs[index].Move(disIndex);
+
+    	}
+    }
+
---
+++ HW3/Assets/Scripts/npcMove.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using System.Threading.Tasks;
+
+public class npcMove : MonoBehaviour
+{
+
+	[SerializeField]
+	Transform[] distenations; 
+
+	public NavMeshAgent navMeshAgent; 
+
+	private int currentIndex ; 
+
+	//private bool running= false;
+	public bool moving = false ; 
+	public Animator npcAnim;
+
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+     navMeshAgent = this.GetComponent<NavMeshAgent>();
+     npcAnim =this.gameObject.transform.GetChild(0).gameObject.GetComponent<Animator>();
+  	 npcAnim.SetBool("isWalking", false);	
+  	 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+		
+        
+    }
+
+    public bool tryStop(){
+    	if (moving&& navMeshAgent.remainingDistance<1f){		
+				moving = false ;
+				npcAnim.SetBool("isWalking", false);	
+				return true ;
+	    	}	
+
+	    return false ;
+    }
+
+ 
+    public void Move (int index){
+    	moving=true ; 
+    	currentIndex= index; 
+    	Vector3 target = distenations[currentIndex].transform.position; 
+    	navMeshAgent.SetDestination(target);
+       	npcAnim.SetBool("isWalking", true);
+    }
+
+    public int GetCurrentDistenationIndex(){
+    	return currentIndex;
+    }
+
+    public int GetDistenationLength(){
+    	return distenations.Length;
+    }
+}
