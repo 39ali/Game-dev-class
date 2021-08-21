af2083b15246ad49626bcfe211a5706a255a8fd5
Ali Milhim <ali.milhim@powtoon.com>
2021-08-21 23:55:14 +0300

    Added finalProject

---
+++ FinalProject/Assets/Scripts/GunShooting.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunShooting : MonoBehaviour
+{
+    public GameObject GunInHand;
+    public GameObject GrenadeInHand;
+    public GameObject aCamera;
+    [SerializeField]
+    public GameObject target;
+    private LineRenderer lr;
+    [SerializeField]
+    public GameObject MuzzleEnd;
+    [SerializeField]
+    private AudioSource sound;
+    [SerializeField]
+    public ParticleSystem MuzzleFlash;
+    [SerializeField]
+    public GameObject Enemy;
+    private float range = 500;
+    //  private Vector3 dir = new Vector3(1);
+    // Start is called before the first frame update
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+        sound = GunInHand.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("TouchBtn") && GunInHand.activeSelf)
+        {
+            Debug.Log("shoot");
+            // RaycastHit hit;
+            //   if(Physics.Raycast(aCamera.transform.position, aCamera.transform.forward, out hit,range))
+            // {
+            //   Debug.Log(hit.transform.name);
+            //   target.transform.position = hit.point;
+            StartCoroutine(ShowShot());
+            // if(hit.transform.gameObject.name==Enemy.gameObject.name)
+            //  {
+            //      Animator a = Enemy.GetComponent<Animator>();
+            //      a.SetBool("IsDying", true);
+            //   }
+            //}
+        }
+    }
+    public IEnumerator ShowShot()
+    {
+        //  Debug.Log(aCamera.transform.forward);
+
+        var ray = new Ray(aCamera.transform.position, aCamera.transform.forward);
+        //  Debug.Log("print this:");
+        // D/ebug.Log(aCamera.transform.position);
+        // Debug.Log(aCamera.transform.forward);
+        //  Debug.Log( MuzzleEnd.transform.forward);
+        lr.SetPosition(0, ray.origin);
+        lr.SetPosition(1, ray.GetPoint(100));
+        //   lr.SetPosition(1, MuzzleEnd.transform.position+ aCamera.transform.forward*range);
+        lr.enabled = true;
+        // target.SetActive(true);
+        MuzzleFlash.Play();
+        sound.Play();
+        yield return new WaitForSeconds(0.1f);
+        lr.enabled = false;
+        // target.SetActive(false);
+    }
+
+
+    public void SetGunActive(bool g)
+    {
+        GunInHand.SetActive(g);
+    }
+    public void SetGrenadeActive(bool g)
+    {
+        GrenadeInHand.SetActive(g);
+    }
+}
---
+++ FinalProject/Assets/Scripts/PickGun.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickGun : MonoBehaviour
+{
+    public GameObject currentPlayer;
+    private int _rotationSpeed = 30;
+    private void OnMouseDown()
+    {
+        if (currentPlayer.tag == "Player")
+        {
+            Debug.Log(gameObject.name + " was picked by Player");
+            gameObject.SetActive(false);
+            if (gameObject.name.Contains("Gun"))
+            {
+                currentPlayer.GetComponent<GunShooting>().SetGunActive(true);
+            }
+            else if (gameObject.name.Contains("m26"))
+            {
+                currentPlayer.GetComponent<GunShooting>().SetGrenadeActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Npc") // only Player can pick coins
+        {
+            this.gameObject.SetActive(false); // turn THIS off
+            npcMove npc = other.transform.gameObject.GetComponent<npcMove>();
+            if (gameObject.name.Contains("Gun"))
+            {
+                npc.SetGunActive(true);
+            }
+            else if (gameObject.name.Contains("m26"))
+            {
+                npc.SetGrenadeActive(true);
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Rotate(0, _rotationSpeed * Time.deltaTime, 0);
+    }
+}
